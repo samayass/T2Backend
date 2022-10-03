afa47ae7a8d539706bc54cd8023504841e79c91e
Iris Yang <irisyangdoudou@gmail.com>
2022-10-03 00:14:18 -0700

    api

---
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/Api-Iris.java
@@ -0,0 +1,43 @@
+package com.nighthawk.spring_portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.json.simple.parser.ParseException;
+
+import org.springframework.ui.Model;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Controller // HTTP requests are handled as a controller, using the @Controller annotation
+public class Api {
+
+    // CONTROLLER handles GET request for /birds, maps it to birds() method
+    @GetMapping("/api")
+    public String api(Model model) throws IOException, InterruptedException, ParseException {
+        HttpRequest request = HttpRequest.newBuilder()
+		.uri(URI.create("https://tennisapi1.p.rapidapi.com/api/tennis/search/nadal"))
+		.header("X-RapidAPI-Key", "ebb6f1a376mshadb02bb5506cb0fp127f56jsncb975393d040")
+		.header("X-RapidAPI-Host", "tennisapi1.p.rapidapi.com")
+		.method("GET", HttpRequest.BodyPublishers.noBody())
+		.build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+
+        // alternative #2: convert response.body() to JSON object
+        Object obj = new JSONParser().parse(response.body());
+        JSONObject jobject = (JSONObject) obj;
+        System.out.println(jobject);
+
+        // pass stats to view
+        model.addAttribute("jobject", jobject);
+
+        return "api";
+
+    }
+}
