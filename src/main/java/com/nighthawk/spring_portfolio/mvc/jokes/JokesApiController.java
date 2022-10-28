29db2063a18bc800ffba1e1c9a5d93d39f1a0e0f
Samaya <samaya2024@gmail.com>
2022-10-28 09:37:35 -0700

    updates database

--- src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/JokesApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/jokes/JokesApiController.java
@@ -5,22 +5,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @RestController // annotation to simplify the creation of RESTful web services
-@RequestMapping("/api/jokes")
+@RequestMapping("/api/jokes")  // all requests in file begin with this URI
 public class JokesApiController {
 
-    // Autowired enables Control to connect HTML and POJO Object to database easily for CRUD operations
+    // Autowired enables Control to connect URI request and POJO Object to easily for Database CRUD operations
     @Autowired
     private JokesJpaRepository repository;
 
-    /*
-    GET List of Jokes
+    /* GET List of Jokes
+     * @GetMapping annotation is used for mapping HTTP GET requests onto specific handler methods.
      */
-    @GetMapping("/all")
+    @GetMapping("/")
     public ResponseEntity<List<Jokes>> getJokes() {
-        return new ResponseEntity<>( repository.findAllByOrderByJokeAsc(), HttpStatus.OK);
+        // ResponseEntity returns List of Jokes provide by JPA findAll()
+        return new ResponseEntity<>( repository.findAll(), HttpStatus.OK);
     }
 }
